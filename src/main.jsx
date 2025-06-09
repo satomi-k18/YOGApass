8bf26a7ea9570fb1278610a90d8b2e3040143cca
satomi-k18 <s420281@gmail.com>
2025-06-09 16:00:06 +0900

    Fix merge conflicts in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // Tailwind CSS
->>>>>>> 02abac037f7bca105f4d710872bf81b9f6885958
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
 )
-=======
-);
->>>>>>> 02abac037f7bca105f4d710872bf81b9f6885958
